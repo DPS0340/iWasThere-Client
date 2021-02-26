9d413f2592796371f97f53b6c472d0c375a0d07c
jiho lee <optional.int@kakao.com>
2021-02-26 12:04:49 +0900

    Improve: MapView Runnable

--- app/src/main/java/xyz/dps0340/iwasthere/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/xyz/dps0340/iwasthere/ui/dashboard/DashboardFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import xyz.dps0340.iwasthere.R
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
@@ -16,6 +21,18 @@ class DashboardFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val mapFragment = parentFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return view
     }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap ?: return
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )    }
 }
